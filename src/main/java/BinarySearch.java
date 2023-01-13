6485802881aa41dee1359c3c18141c458ac64ec8
Evan Merritt <109871289+MrMerrittx702@users.noreply.github.com>
2023-01-12 22:20:23 -0600

    Update BinarySearch.java

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -1,1 +1,22 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BinarySearch {
+
+//Arrays
+public static int binarySearch(int[] array, int searchedElem){
+//implement this method
+}
+//ArrayList
+public static int binarySearch(ArrayList<Integer> list, int searchedElem){
+  //implement this method
+}
+  public static void main(String[] args){
+    //use the following to test your code for accuracy
+    int[] array = {2,12,14,25,54,67,89,96}
+    ArrayList<Integer> list = new ArrayList<>(Arrays.asList(14,34,42,66,79,81,90,100));
+
+
+  }
+}
